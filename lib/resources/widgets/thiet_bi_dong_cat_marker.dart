0a9f624a297302a7b4e9d0e0ebe109c88c27c787
quy.doanduy <quy.doan@vietgis.com.vn>
2022-06-22 14:32:26 +0700

    a

--- lib/resources/widgets/thiet_bi_dong_cat_marker.dart
+++ lib/resources/widgets/thiet_bi_dong_cat_marker.dart
@@ -13,27 +13,49 @@ class ThietBiDongCatWidgetMarker extends StatelessWidget {
       child: Column(
         children: [
           Text(
-            (attributes['THIETBI'] != null
-                ? attributes['THIETBI'] as String
+            (attributes.entries.first != null
+                ? attributes.entries.first.value as String
                 : (attributes['OBJECTID'] as int).toString()),
             style: const TextStyle(
                 color: Colors.black, fontWeight: FontWeight.bold),
           ),
           Image.asset(
-            attributes['CONGSUAT'] == null
-                ? getImageAsset('map/thiet_bi_dong_cat/dao_cach_ly.png')
-                : double.tryParse((attributes['CONGSUAT'] as String)) == 110
-                    ? getImageAsset('map/tram_bien_ap/110.png')
-                    : double.tryParse((attributes['CONGSUAT'] as String)) == 220
-                        ? getImageAsset('map/tram_bien_ap/220.png')
-                        : double.tryParse((attributes['CONGSUAT'] as String)) ==
-                                500
-                            ? getImageAsset('map/tram_bien_ap/500.png')
-                            : getImageAsset('map/cot_dien/other.png'),
+            getImageAsset('map/cot_dien/other.png'),
             width: 40,
           ),
         ],
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return FittedBox(
+  //     fit: BoxFit.scaleDown,
+  //     child: Column(
+  //       children: [
+  //         Text(
+  //           (attributes['THIETBI'] != null
+  //               ? attributes['THIETBI'] as String
+  //               : (attributes['OBJECTID'] as int).toString()),
+  //           style: const TextStyle(
+  //               color: Colors.black, fontWeight: FontWeight.bold),
+  //         ),
+  //         Image.asset(
+  //           attributes['CONGSUAT'] == null
+  //               ? getImageAsset('map/thiet_bi_dong_cat/dao_cach_ly.png')
+  //               : double.tryParse((attributes['CONGSUAT'] as String)) == 110
+  //                   ? getImageAsset('map/tram_bien_ap/110.png')
+  //                   : double.tryParse((attributes['CONGSUAT'] as String)) == 220
+  //                       ? getImageAsset('map/tram_bien_ap/220.png')
+  //                       : double.tryParse((attributes['CONGSUAT'] as String)) ==
+  //                               500
+  //                           ? getImageAsset('map/tram_bien_ap/500.png')
+  //                           : getImageAsset('map/cot_dien/other.png'),
+  //           width: 40,
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 }

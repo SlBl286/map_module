6753d3ede36469f3b1a96c4da7676ca6ed8a6e24
quy.doanduy <quy.doan@vietgis.com.vn>
2022-06-22 14:56:41 +0700

    a

---
+++ lib/resources/widgets/map_maker.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:nylo_framework/nylo_framework.dart';
+
+class MarkerWidget extends StatelessWidget {
+  Map<String, dynamic> attributes;
+  MarkerWidget({Key? key, required this.attributes})
+      : MarkerWidget(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      fit: BoxFit.scaleDown,
+      child: Column(
+        children: [
+          Text(
+            (attributes['TENTRAMBIENAP'] != null
+                ? attributes.entries.first.value.toString()
+                : (attributes['OBJECTID'] as int).toString()),
+            style: const TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          Image.asset(
+           
+                            getImageAsset('map/tram_bien_ap/other.png'),
+            width: 50,
+          ),
+        ],
+      ),
+    );
+  }
+}
